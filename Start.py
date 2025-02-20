e8d7185db096ae8600c56824328fe66675b927dd
adrianaheeb <adrianaheeb@bluewin.ch>
2025-02-20 14:35:12 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Adriana Heeb entwickelt.")
- st.write("E-Mail Adresse: heebadr1@students.zhaw.ch")
+st.write("E-Mail Adresse: heebadr1@students.zhaw.ch")
