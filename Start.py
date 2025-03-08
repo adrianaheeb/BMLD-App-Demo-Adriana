be2bcbae04fa6c257f5c2bacfb43f68582e2d9b0
adrianaheeb <heebadr1@students.zhaw.ch>
2025-03-08 20:39:51 +0100

    Anpassungen

--- Start.py
+++ Start.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("BFI-App")
-st.write("Diese Streamlit-App bietet eine einfache Möglichkeit, den Körperfettanteil zu berechnen, basierend auf grundlegenden persönlichen Angaben wie Größe, Gewicht, Alter, Geschlecht sowie Aktivitätslevel. Die benutzerfreundliche Oberfläche ermöglicht es, eine erste Einschätzung der Körperzusammensetzung schnell zu erhalten.")
+st.write("Diese Streamlit-App bietet eine einfache Möglichkeit, den Körperfettanteil zu berechnen, basierend auf grundlegenden persönlichen Angaben wie Grösse, Gewicht, Alter, Geschlecht sowie Aktivitätslevel. Die benutzerfreundliche Oberfläche ermöglicht es, eine erste Einschätzung der Körperzusammensetzung schnell zu erhalten.")
 
 st.write("Diese App wurde von Adriana Heeb entwickelt.")
 st.write("E-Mail Adresse: heebadr1@students.zhaw.ch")
