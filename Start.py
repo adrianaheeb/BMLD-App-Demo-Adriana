c103558af32ff6820f15dee3b4757bc50e227f6e
adrianaheeb <heebadr1@students.zhaw.ch>
2025-03-16 17:59:35 +0100

    emoji dazugefügt

--- Start.py
+++ Start.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 st.title("BFI-App")
 st.write("Diese Streamlit-App bietet eine einfache Möglichkeit, den Körperfettanteil zu berechnen, basierend auf grundlegenden persönlichen Angaben wie Grösse, Gewicht, Alter, Geschlecht sowie Aktivitätslevel. Die benutzerfreundliche Oberfläche ermöglicht es, eine erste Einschätzung der Körperzusammensetzung schnell zu erhalten.")
+st.write("🏃")
 
 st.write("Diese App wurde von Adriana Heeb entwickelt.")
 st.write("E-Mail Adresse: heebadr1@students.zhaw.ch")
+
+
